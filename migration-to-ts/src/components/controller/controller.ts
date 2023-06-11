7ede8f8b46ca10e2188c81290ee1ab725e48d6f5
aniretakey <EMKornishina@gmail.com>
2023-06-11 11:31:44 +0300

    feat: migrate controller.js

---
+++ migration-to-ts/src/components/controller/controller.ts
@@ -0,0 +1,46 @@
+import AppLoader from './appLoader';
+import { Callback } from '../../types/types';
+
+class AppController extends AppLoader {
+  public getSources(callback: Callback): void {
+    super.getResp(
+      {
+        endpoint: 'sources',
+      },
+      callback
+    );
+  }
+
+  public getNews(e: Event, callback: Callback): void {
+    let target: EventTarget | null = e.target;
+    const newsContainer = e.currentTarget;
+
+    while (target !== newsContainer) {
+      if (target && target instanceof HTMLElement) {
+        if (target.classList.contains('source__item')) {
+          const sourceId = target.getAttribute('data-source-id');
+          if (newsContainer && newsContainer instanceof HTMLElement) {
+            if (newsContainer.getAttribute('data-source') !== sourceId) {
+              if (sourceId) {
+                newsContainer.setAttribute('data-source', sourceId);
+                super.getResp(
+                  {
+                    endpoint: 'everything',
+                    options: {
+                      sources: sourceId,
+                    },
+                  },
+                  callback
+                );
+              }
+            }
+          }
+          return;
+        }
+        target = target.parentNode;
+      }
+    }
+  }
+}
+
+export default AppController;
