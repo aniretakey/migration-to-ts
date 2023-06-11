f2a5b83b31e4a9aeea246ba6d2d00f1532d07225
aniretakey <EMKornishina@gmail.com>
2023-06-11 18:14:17 +0300

    feat: migrate app.js

---
+++ migration-to-ts/src/components/app/app.ts
@@ -0,0 +1,27 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import { safeQuerySelector } from '../../components/view/news/news';
+
+class App {
+  private controller: AppController;
+  private view: AppView;
+  constructor() {
+    this.controller = new AppController();
+    this.view = new AppView();
+  }
+
+  public start(): void {
+    if (document && document !== null) {
+      safeQuerySelector('.sources', document).addEventListener('click', (e) =>
+        this.controller.getNews(e, (data) => {
+          this.view.drawNews(data);
+        })
+      );
+      this.controller.getSources((data) => {
+        this.view.drawSources(data);
+      });
+    }
+  }
+}
+
+export default App;
